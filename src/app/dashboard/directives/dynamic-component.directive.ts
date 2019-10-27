9a4770db87221fbbaed7ded2bf26cf2cd1b84021
Alexander Pavlov <alexander.pavlov.development@gmail.com>
2019-10-27 22:16:49 +0200

    Update dynamic-component.directive.ts

--- src/app/dashboard/directives/dynamic-component.directive.ts
+++ src/app/dashboard/directives/dynamic-component.directive.ts
@@ -1,42 +1,42 @@
 import { Directive, ViewContainerRef, Input, ComponentFactoryResolver, OnInit, EventEmitter, ComponentRef } from '@angular/core';
 
 @Directive({
-    selector: '[dynamic-component]',
+  selector: '[appDynamicComponent]',
 })
 export class DynamicComponentDirective implements OnInit {
-    @Input()
-    component: any;
+  @Input()
+  component: any;
 
-    @Input()
-    inputs: { [name: string]: any; }[] = [];
+  @Input()
+  inputs: { [name: string]: any; }[] = [];
 
-    @Input()
-    outputs: { [name: string]: any; }[] = [];
+  @Input()
+  outputs: { [name: string]: any; }[] = [];
 
-    constructor(public viewContainerRef: ViewContainerRef, private resolver: ComponentFactoryResolver) { }
+  constructor(public viewContainerRef: ViewContainerRef, private resolver: ComponentFactoryResolver) { }
 
-    ngOnInit() {
-        const componentFactory = this.resolver.resolveComponentFactory<any>(this.component);
-        const componentRef = this.viewContainerRef.createComponent<any>(componentFactory);
+  ngOnInit() {
+    const componentFactory = this.resolver.resolveComponentFactory<any>(this.component);
+    const componentRef = this.viewContainerRef.createComponent<any>(componentFactory);
 
-        this.SetComponentInputs(componentRef);
-        this.SetComponentOutputs(componentRef);
+    this.SetComponentInputs(componentRef);
+    this.SetComponentOutputs(componentRef);
 
-        this.viewContainerRef.insert(componentRef.hostView);
+    this.viewContainerRef.insert(componentRef.hostView);
+  }
+
+  private SetComponentOutputs(componentRef: ComponentRef<any>) {
+    for (const [eventName, eventHandler] of Object.entries(this.outputs)) {
+      if (eventName in componentRef.instance) {
+        const eventEmitter: EventEmitter<any> = componentRef.instance[eventName];
+        eventEmitter.subscribe(eventHandler);
+      }
     }
+  }
 
-    private SetComponentOutputs(componentRef: ComponentRef<any>) {
-        for (const [eventName, eventHandler] of Object.entries(this.outputs)) {
-            if (eventName in componentRef.instance) {
-                const eventEmitter: EventEmitter<any> = componentRef.instance[eventName];
-                eventEmitter.subscribe(eventHandler);
-            }
-        }
+  private SetComponentInputs(componentRef: ComponentRef<any>) {
+    for (const [key, value] of Object.entries(this.inputs)) {
+      componentRef.instance[key] = value;
     }
-
-    private SetComponentInputs(componentRef: ComponentRef<any>) {
-        for (const [key, value] of Object.entries(this.inputs)) {
-            componentRef.instance[key] = value;
-        }
-    }
+  }
 }
