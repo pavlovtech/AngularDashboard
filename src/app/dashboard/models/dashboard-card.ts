f77254494fd5162e67775e0314bbe17c927e0616
Alexander Pavlov <alexander.pavlov.development@gmail.com>
2019-10-29 18:27:54 +0200

    Fix for the DashboardCard initialization

--- src/app/dashboard/models/dashboard-card.ts
+++ src/app/dashboard/models/dashboard-card.ts
@@ -2,31 +2,31 @@ import { GridsterItem } from 'angular-gridster2';
 import * as uuid from 'uuid/v1';
 
 export class DashboardCard implements GridsterItem {
-    cardId?: string;
-    component?: any;
-    componentName?: string;
-    componentInstance?: any;
-    x?: number;
-    y?: number;
-    rows?: number;
-    cols?: number;
-    dragEnabled?: boolean;
-    resizeEnabled?: boolean;
-    compactEnabled?: boolean;
-    maxItemRows?: number;
-    minItemRows?: number;
-    maxItemCols?: number;
-    minItemCols?: number;
-    minItemArea?: number;
-    maxItemArea?: number;
+  cardId?: string;
+  component?: any;
+  componentName?: string;
+  componentInstance?: any;
+  x?: number;
+  y?: number;
+  rows?: number;
+  cols?: number;
+  dragEnabled?: boolean;
+  resizeEnabled?: boolean;
+  compactEnabled?: boolean;
+  maxItemRows?: number;
+  minItemRows?: number;
+  maxItemCols?: number;
+  minItemCols?: number;
+  minItemArea?: number;
+  maxItemArea?: number;
 
-    constructor(card: DashboardCard) {
-        Object.assign(this, card);
+  constructor(card: DashboardCard) {
+      this.componentName = this.component.name;
 
-        this.componentName = this.component.name;
+      if (!this.cardId) {
+        this.cardId = uuid();
+      }
 
-        if (!this.cardId) {
-            this.cardId = uuid();
-        }
-    }
+      Object.assign(this, card);
+  }
 }
